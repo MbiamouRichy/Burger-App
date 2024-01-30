310c27dc03f746189f2aa4bbcb8dbb59b45c385d
MbiamouRichy <richymbiamou52@gmail.com>
2024-01-30 19:47:35 +0100

    ;ise en plqce de tout le preojet

--- main.js
+++ main.js
@@ -3,18 +3,51 @@ let accueil_vue = document.querySelector(".accueil-vue");
 let retour = document.querySelector(".retour");
 let btn_right = document.querySelector(".btn-right");
 let btn_left = document.querySelector(".btn-left");
+let slides = document.querySelectorAll(".slide");
+let inds = document.querySelectorAll(".ind");
+let DivSlides = document.querySelector(".slider");
+let indicateur = document.querySelector(".indicateur");
+let activeSlide = 0;
+let activeSlideAnimate = 0;
 
 div_expore.addEventListener("click", () => {
   accueil_vue.className += " hidden";
   retour.classList.remove("hidden");
-  div_expore.classList.add("hidden");
   btn_left.classList.remove("hidden");
   btn_right.classList.remove("hidden");
+  DivSlides.classList.remove("hidden");
+  div_expore.classList += " hidden";
+  indicateur.classList.remove("hidden");
 });
 retour.addEventListener("click", () => {
   accueil_vue.classList.remove("hidden");
-  retour.classList.add("hidden");
-  btn_left.classList.add("hidden");
-  btn_right.classList.add("hidden");
+  btn_left.classList = " hidden";
+  btn_right.classList += " hidden";
   div_expore.classList.remove("hidden");
+  DivSlides.classList += " hidden";
+  retour.classList += " hidden";
+  indicateur.classList.add("hidden");
 });
+
+// Navigation du slide
+btn_left.addEventListener("click", () => {
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+  changeSlide();
+});
+btn_right.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+  changeSlide();
+});
+
+function changeSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  inds.forEach((ind) => ind.classList.remove("active"));
+  slides[activeSlide].className += " active";
+  inds[activeSlide].className += " active";
+}
